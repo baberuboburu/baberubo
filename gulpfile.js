2cbe2c0effbb58f59a0c8ec1f56cebf2fe843f1a
Aoto Sasaki <91579093+baberuboburu@users.noreply.github.com>
2022-06-02 20:20:24 +0900

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const ejs = require("gulp-ejs");
+const replace = require("gulp-replace"); 
+
+gulp.task("ejs", done => {
+  gulp
+    .src(["ejs/**/*.ejs", "!ejs/**/_*.ejs"])
+    .pipe(ejs({}, {}, { ext: ".html" }))
+    .pipe(rename({ extname: ".html" }))
+    .pipe(replace(/[\s\S]*?(<!DOCTYPE)/, "$1"))
+    .pipe(gulp.dest("./"));
+  done();
+});
